6d0c64b8a89440c740856acff8622109612f7e8f
sonelu <alex.sonea@gmail.com>
2016-08-26 22:55:45 +0100

    primitives

--- software/poppy_seed/poppy_seed.py
+++ software/poppy_seed/poppy_seed.py
@@ -6,12 +6,12 @@ from functools import partial
 
 from poppy.creatures import AbstractPoppyCreature
 
+from .primitives.posture import StandPosition, SitPosition
 """
 we'll deal with these later
 
 from .primitives.safe import LimitTorque, TemperatureMonitor
 from .primitives.dance import SimpleBodyBeatMotion
-from .primitives.posture import StandPosition, SitPosition
 from .primitives.idle import UpperBodyIdleMotion, HeadIdleMotion
 from .primitives.interaction import ArmsTurnCompliant, PuppetMaster
 """
